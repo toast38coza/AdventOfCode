cda2d9beb726054984b5fba2da8e28add11c2dae
toast38coza <info@38.co.za>
2016-01-15 09:16:11 +0200

    Python answer for Q3 part two. + refactor to use a Santa class.

--- 3/python3/delivery.py
+++ 3/python3/delivery.py
@@ -8,39 +8,84 @@ Follow directions. Place each new location into a set
 Find the length of the set
 """
 
-def move(direction, current_position): 
+class Santa:
+
+	def __init__(self):
+		self.houses_visited = set(["0:0"])
+		self.current_position = [0,0]
+
+	def __save(self):
+		position_string = "{}:{}" . format (self.current_position[0], self.current_position[1])
+		self.houses_visited.add(position_string)
+
+	def move(self, direction): 
+		
+		direction_map = {
+			# direction: (co-ordinate (up/down or east/west), positive or negative) 
+			">": (1,1),
+			"<": (1,-1),
+			"^": (0,1),
+			"v": (0,-1),
+		}
+		co_ordinate = direction_map[direction][0]
+		direction = direction_map[direction][1]
+		self.current_position[co_ordinate] += direction
+
+		"""
+		# the map feels a little more elegant, 
+		# but the if's are a little easier to read
+		if direction == ">":
+			current_position[1] += 1 
+		if direction == "<":
+			current_position[1] -= 1 
+		if direction == "^":
+			current_position[0] += 1 
+		if direction == "v":
+			current_position[0] -= 1 
+		"""
+		self.__save()
+		return self.current_position
+
 	
-	if direction == ">":
-		current_position[1] += 1 
-	if direction == "<":
-		current_position[1] -= 1 
-	if direction == "^":
-		current_position[0] += 1 
-	if direction == "v":
-		current_position[0] -= 1 
 
-	return current_position
-
-def save(position, houses_visited):
-	position_string = "{}:{}" . format (position[0], position[1])
-	houses_visited.add(position_string)
-	return houses_visited
-
-def get_houses(input):
+def get_single_santa_trip(input):
 	
-	houses_visited = set(["0:0"])
-	current_position = [0,0]
-
+	santa = Santa()
 	for char in input:
-		new_position = move(char, current_position)
-		houses_visited = save(current_position, houses_visited)
+		santa.move(char)
 
-	return len(houses_visited)
+	return len(santa.houses_visited)
+
+def count_houses_with_robo_santa(input):
+
+	santa = Santa()
+	robo_santa = Santa()
+	for index, char in enumerate(input):
+		if index%2 == 0:
+			santa.move(char)
+		else:
+			robo_santa.move(char)
+
+	santa_houses = len(santa.houses_visited)
+	robo_houses = len(robo_santa.houses_visited)
+
+	# this is the simple way of doing it (handles this specific case)
+	# more efficient, but less _correct_
+	# return santa_houses + robo_houses -1
+
+	# this is a little less efficient (you have to union two sets which is: O(len(s)+len(t)) )
+	# but does make this method more reliable/flexible and less prone to surprising outputs
+	return len(santa.houses_visited.union(robo_santa.houses_visited))
+
 
 def main():
 	input = open('../input.txt', 'r').read()
-	answer = get_houses(input)
-	print answer
+	part_one_answer = get_single_santa_trip(input)
+	print "Houses visited by a single santa: {}" . format (part_one_answer)
+
+	part_two_answer = count_houses_with_robo_santa(input)
+	print "Houses visited by santa and robo-santa: {}" . format (part_two_answer)
+
 
 if __name__ == '__main__':
     main()			
--- 3/python3/test_delivery.py
+++ 3/python3/test_delivery.py
@@ -1,9 +1,16 @@
 import unittest 
-from delivery import get_houses, move, save
+from delivery import Santa, get_single_santa_trip, count_houses_with_robo_santa
 
-class DeliveryTestCase(unittest.TestCase):
+class SantaTestCase(unittest.TestCase):
+
+	def test_init_santa(self):
+		"""A new santa object is correctly setup"""
+		santa = Santa()
+		assert santa.houses_visited == set(["0:0"])
+		assert santa.current_position == [0,0]
 
 	def test_move(self):
+		"""Should correctly change position according to instructions from the elf"""
 
 		test_inputs = [
 			(">", [0,1]),
@@ -13,11 +20,16 @@ class DeliveryTestCase(unittest.TestCase):
 		]
 
 		for input, expected_result in test_inputs:
-			new_position = move(input, [0,0])
+			santa = Santa()			
+			new_position = santa.move(input)
+			
 			assert new_position == expected_result, \
-				"Expect {} to be {}". format(new_position, expected_result)
+				"Expect {} to be {} for {}". format(new_position, expected_result, input)
 
-	def test_delivery(self):
+class DeliveryTestCase(unittest.TestCase):
+
+	def test_get_single_santa_trip(self):
+		"""Should correctly count the number of houses visited by a single santa instance"""
 
 		test_inputs = [
 			(">", 2),
@@ -26,9 +38,24 @@ class DeliveryTestCase(unittest.TestCase):
 		]
 
 		for input, expected_result in test_inputs:
-			num_houses = get_houses(input)
+			num_houses = get_single_santa_trip(input)
 			assert num_houses == expected_result, \
-			"Expected {} to be: {}" . format (num_houses, expected_input)
+			"Expected {} to be: {}" . format (num_houses, expected_result)
+
+	def test_get_santa_and_robo_santa_trip(self):
+		"""Should correctly count the number of houses visited by a santa and robo-santa collaborative effort"""
+
+		test_inputs = [
+			(">", 2),
+			("^>v<", 3),
+			("^v^v^v^v^v", 11),
+		]
+
+		for input, expected_result in test_inputs:
+			num_houses = count_houses_with_robo_santa(input)
+			assert num_houses == expected_result, \
+			"Expected {} to be: {} for {}" . format (num_houses, expected_result, input)
+
 
 if __name__ == '__main__':
     unittest.main()
